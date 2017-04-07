c7343cf84d825367754dde1f5a2d47696a72de83
muyuran <337304568@qq.com>
2017-04-07 13:33:39 +0800

    need test

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from wxpy import *
 # 声明成员
 
 bot = Bot()
-# Friends_test = bot.friends().search(u'东东')[0]
-group_geek = bot.groups().search(u'Geek')[0]
+# Friends_test = bot.friends().search('东东')[0]
+group_geek = bot.groups().search('算法第6群')[0]
 
 #  logger = get_wechat_logger()
-#  logger.warning(u'這是一條WARNING等級的日志，你收到了嗎？')
+#  logger.warning('這是一條WARNING等級的日志，你收到了嗎？')
 
 
 # 回复群里@到和好友信息
@@ -22,7 +22,7 @@ def auto_reply_text(msg):
         return 
     else:
         return '收到信息: {} ({})'.format(msg.text, msg.type)
-        if u'加群' in msg.text.lower():
+        if '加群' in msg.text.lower():
             add_members(msg.sender,use_invitation=True)
 
 
@@ -40,16 +40,16 @@ def auto_reply_pic(msg):
 
 @bot.register(msg_types=FRIENDS)
 def add_friend(msg):
-    if u'每日一题' in msg.text.lower():
+    if '我要加群' in msg.text.lower():
         new_friend = bot.accept_friend(msg.card)
-        new_friend.send(u'每日一题微信机器人为您服务', msg.sender)
-
+        new_friend.send('每日一题微信机器人为您服务', msg.sender)
+        group_geek.add_members(msg.sender, use_invitation=True)
 
 # 自动拉人进群
 
 @bot.register(Friend, TEXT)
 def add_group_member(msg):
-    if u'申请入群' in msg.text.lower():
+    if '申请入群' in msg.text.lower():
         group_geek.add_members(msg.sender, use_invitation=True)
 
 bot.start()
