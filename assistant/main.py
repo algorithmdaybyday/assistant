8a974fb2c2bcc5530246e5674491b7221405475f
XudongHe <hexudong08@gmail.com>
2017-04-09 21:52:29 +0800

    增强了机器人的功能

--- assistant/main.py
+++ assistant/main.py
@@ -2,20 +2,30 @@
 #coding=utf-8
 import json
 from wxpy import *
+import threading
+import time
 
 
 # 声明成员
+bot = Bot(cache_path=True, console_qr=True)
 
-bot = Bot()
-# Friends_test = bot.friends().search('东东')[0]
-group_geek = bot.groups().search('每日一题6群')[0]
+# 定义同步消息的groups
+global groups
+global group
+global group2
+groups = bot.groups().search('算法')
+group = bot.groups().search("每日一题算法6群")[0]
+group2 = bot.groups().search("每日一题中转站")[0]
 
 #  logger = get_wechat_logger()
 #  logger.warning('這是一條WARNING等級的日志，你收到了嗎？')
 
+# 同步微信群的消息
+@bot.register(groups, except_self=False)
+def sync_my_groups(msg):
+    sync_message_in_groups(msg, groups)
 
 # 回复群里@到和好友信息
-
 @bot.register([Friend, Group], TEXT)
 def auto_reply_text(msg):
     if isinstance(msg.chat, Group) and not msg.is_at:
@@ -27,7 +37,6 @@ def auto_reply_text(msg):
 
 
 # 发送图片
-
 @bot.register([Friend, Group], PICTURE)
 def auto_reply_pic(msg):
     if isinstance(msg.chat, Group) and not msg.is_at:
@@ -35,63 +44,39 @@ def auto_reply_pic(msg):
     else:
         msg.reply_image('./assets/th.jpg', media_id=None)
 
-
 # 自动添加好友
-
 @bot.register(msg_types=FRIENDS)
 def add_friend(msg):
-    if '我要加群' in msg.text.lower():
-        new_friend = bot.accept_friend(msg.card)
-        new_friend.send('每日一题微信机器人为您服务', msg.sender)
-        group_geek.add_members(msg.sender, use_invitation=True)
+    message = '''
+欢迎加入每日一题群，我是加群助手小雪。
+每日一题群是每天练习一道算法题的算法学习群。
+历史题目和总结见公众号「每日一道算法题」, 或者搜索 myhuyanluanyu 。
+        '''
+    new_friend = bot.accept_friend(msg.card)
+    new_friend.send(message, msg.sender)
+    group.add_members(msg.sender, use_invitation=True)
 
 # 自动拉人进群
-
 @bot.register(Friend, TEXT)
 def add_group_member(msg):
-    if '申请入群' in msg.text.lower():
-        group_geek.add_members(msg.sender, use_invitation=True)
+    if '刷题' in msg.text.lower() or '加群' in msg.text.lower():
+        group.add_members(msg.sender, use_invitation=True)
 
-'''
-def createDaemon():
-    # fork 进程
-    try:
-        if os.fork() > 0: os._exit(0)
-    except OSError:
-        print('fork #1 failed: %d (%s)' % (error.errno.strerror))
-        os._exit(1)
-    os.chdir('/')
-    os.setsid()
-    os.umask(0)
-    try:
-        pid = os.fork()
-        if pid > 0:
-            print('Daemon PID %d' % pid)
-            os._exit(0)
-    except OSError:
-        print('fork #2 failed: %d (%s)' % (error.errno.strerror))
-        os._exit(1)
-    # 重定向标准IO
-    sys.stdout.flush()
-    sys.stderr.flush()
-    si = open("/dev/null", 'r')
-    so = open("/dev/null", 'a+')
-    se = open("/dev/null", 'wb', buffering=0)
-    os.dup2(si.fileno(), sys.stdin.fileno())
-    os.dup2(so.fileno(), sys.stdout.fileno())
-    os.dup2(se.fileno(), sys.stderr.fileno())
+def send_message_to_group():
+    message = '''@所有人:
+大家好，我是群助手机器人小雪。
+没进入算法主群的伙伴请加我微信，并发送我要加群的消息给我，我拉你们进主群。
+已经进主群的伙伴麻烦退出该群，谢谢。
+没有收到邀请的伙伴请发如下信息给我：我要刷题
+    '''
+    group2.send(message)
+    global timer
+    timer = threading.Timer(3600, send_message_to_group)
+    timer.start()
 
-    # 在子进程中执行代码
-    bot.start()
-    bot.join()
-
-if __name__ == '__main__':
-    if platform.system() == 'Linux':
-        createDaemon()
-    else:
-        os._exit(0)
-'''
+timer = threading.Timer(3600, send_message_to_group)
+timer.start()
 
 bot.start()
-#embed()
+# embed()
 bot.join()  # 堵塞线程
